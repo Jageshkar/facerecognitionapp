f1dd157adc2995d25863051d37eb8a7ff98d020a
Jageshkar <jageshkar@gmail.com>
2022-05-20 12:39:36 +0530

    typo

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -159,7 +159,7 @@ class App extends Component {
     .then(response => response.json()) 
     .then(response => {
       if(response) {
-        fetch('https://frozen-tor-57173.herokuapp.com:3001/image', {
+        fetch('https://frozen-tor-57173.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
